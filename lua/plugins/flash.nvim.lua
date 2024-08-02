28a5c49b13c158ef936126de63b720aa7b168ea1
Caleb McIrvin <calebmcirvin111@gmail.com>
2024-08-02 00:51:27 -0400

    Added case insensitivity to flash search

--- lua/plugins/flash.nvim.lua
+++ lua/plugins/flash.nvim.lua
@@ -1,7 +1,16 @@
 local plugin = {"folke/flash.nvim"}
 
 plugin.keys = {
-  { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "flash" },
+  { "s", mode = { "n", "x", "o" },
+  function()
+    local gi = vim.go.ignorecase
+    local gs = vim.go.smartcase
+    vim.go.ignorecase = true
+    vim.go.smartcase = false
+    require("flash").jump()
+    vim.go.ignorecase = gi
+    vim.go.smartcase = gs
+  end, desc = "flash" },
   { "f", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "flash treesitter" },
   { "r", mode = "o", function() require("flash").remote() end, desc = "remote flash" }
 }
