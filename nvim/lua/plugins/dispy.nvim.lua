8247fe46907cb678c8a882687f2621f90b6d543b
Caleb McIrvin <calebmcirvin111@gmail.com>
2025-04-24 17:04:46 -0400

    Updated dispy.nvim config

--- nvim/lua/plugins/dispy.nvim.lua
+++ nvim/lua/plugins/dispy.nvim.lua
@@ -10,11 +10,9 @@ function plugin.config()
     cell_aspect_ratio = 18 / 40,
     scale = 1.0,
   })
-  vim.keymap.set({'n', 'v'}, '<leader>pi', ':lua require("dispynvim").display_single_image()<CR>')
 
-  vim.keymap.set({'n', 'v'}, '<leader>pl', ':lua require("dispynvim").display_random_images(4)<CR>')
-  vim.keymap.set({'n', 'v'}, '<leader>ps', ':lua require("dispynvim").plot_statistics()<CR>')
-  vim.keymap.set({'n', 'v'}, '<leader>pt', ':lua require("dispynvim").print_statistics()<CR>')
+  vim.keymap.set({'n', 'v'}, '<leader>px', ':lua require("dispynvim").plot({mode="plot_statistics"})<CR>')
+  vim.keymap.set({'n', 'v'}, '<leader>ps', ':lua require("dispynvim").plot({mode="show_tensor"})<CR>')
 end
 
 return plugin
