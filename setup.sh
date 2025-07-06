ef566e0efba9d7da960b6472eda13c27d77b719b
Caleb McIrvin <calebmcirvin111@gmail.com>
2025-07-06 15:34:01 -0400

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+ln -sf ~/.dotfiles/.bashrc ~/.bashrc
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.profile ~/.profile
+ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/.gitignore ~/.gitignore
+
+ln -sf ~/.dotfiles/nvim ~/.config/nvim
+ln -sf ~/.dotfiles/starship.toml ~/.config/starship.toml
