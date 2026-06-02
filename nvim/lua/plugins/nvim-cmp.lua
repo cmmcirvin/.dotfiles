8dcb9e85b54bca64c58a80371d498d9a5f14c9b2
Caleb McIrvin <calebmcirvin111@gmail.com>
2026-06-02 14:09:44 -0400

    Initial port of UltiSnips to LuaSnip
    Incomplete, many snippets are still missing.

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -2,18 +2,30 @@ local plugin = {'hrsh7th/nvim-cmp'}
 
 plugin.dependencies = {
   -- Sources
-  {'quangnguyen30192/cmp-nvim-ultisnips'},
+  -- {'quangnguyen30192/cmp-nvim-ultisnips'},
+  { 'saadparwaiz1/cmp_luasnip' },
+  {'zbirenbaum/copilot-cmp'},
   {'hrsh7th/cmp-buffer'},
   {'hrsh7th/cmp-path'},
   {'hrsh7th/cmp-nvim-lsp'},
 
   -- Snippets
-  {'sirver/ultisnips'}
+  { 'L3MON4D3/LuaSnip' }
+  -- {'sirver/ultisnips'}
 }
 
 function plugin.config()
   local cmp = require('cmp')
-  local cmp_ultisnips_mappings = require("cmp_nvim_ultisnips.mappings")
+  -- local cmp_ultisnips_mappings = require("cmp_nvim_ultisnips.mappings")
+
+  local luasnip = require("luasnip")
+
+  luasnip.config.setup({ enable_autosnippets = true, })
+
+  require("luasnip.loaders.from_lua").lazy_load({
+    paths = { vim.fn.stdpath("config") .. "/lua/snippets" }
+  })
+
   local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 
   cmp.setup({
@@ -22,25 +34,40 @@ function plugin.config()
       documentation = cmp.config.window.bordered(),
     },
     mapping = {
-      ["<c-j>"] = cmp.mapping(
-        function(fallback)
-          cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
-        end,
-        { "i", "s" }
-      ),
-      ["<c-k>"] = cmp.mapping(
-            function(fallback)
-              cmp_ultisnips_mappings.jump_backwards(fallback)
-            end,
-            { "i", "s" }
-          ),
+      -- ["<c-j>"] = cmp.mapping(
+      --   function(fallback)
+      --     cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
+      --   end,
+      --   { "i", "s" }
+      -- ),
+      -- ["<c-k>"] = cmp.mapping(
+      --       function(fallback)
+      --         cmp_ultisnips_mappings.jump_backwards(fallback)
+      --       end,
+      --       { "i", "s" }
+      --     ),
+      ["<c-j>"] = cmp.mapping(function(fallback)
+        if luasnip.expand_or_locally_jumpable() then
+          luasnip.expand_or_jump()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<c-k>"] = cmp.mapping(function(fallback)
+        if luasnip.locally_jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
       ['J'] = cmp.mapping.scroll_docs(4),
       ['K'] = cmp.mapping.scroll_docs(-4),
       ['<c-l>'] = cmp.mapping.confirm({ select = true }),
       ['<c-h>'] = cmp.mapping.abort(),
     },
     sources = {
-      { name = "ultisnips", group_index=0},
+      -- { name = "ultisnips", group_index=0},
+      { name = "luasnip", group_index=0},
       { name = "copilot", group_index=1},
       { name = "nvim_lsp", group_index=1},
       { name = "path", group_index=2},
@@ -48,7 +75,8 @@ function plugin.config()
     },
     snippet = {
       expand = function(args)
-            vim.fn["UltiSnips#Anon"](args.body)
+        require('luasnip').lsp_expand(args.body)
+        -- vim.fn["UltiSnips#Anon"](args.body)
       end,
     },
     experimental = {
---
+++ nvim/lua/snippets/markdown.lua
@@ -0,0 +1,173 @@
+local ls = require("luasnip")
+
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local sn = ls.snippet_node
+local f = ls.function_node
+
+return {
+  -- Tomorrow checklist (UltiSnips Python eval converted)
+  s({
+      trig = "tmw",
+      desc = "Tomorrow checklist",
+      snippetType = "autosnippet",
+      wordTrig = true,
+    }, {
+      f(function()
+        return os.date("%m/%d/%Y", os.time() + 86400)
+      end),
+      t({
+        "",
+        "- [ ] Cleared Anki",
+        "- [ ] Added to Anki",
+        "- [ ] Gym",
+      }),
+    }),
+  -- Horizontal line
+  s({ trig = "hl", snippetType = "autosnippet" }, {
+      t("---"),
+    }),
+  -- Code block (generic)
+  s({ trig = "mc", snippetType = "autosnippet" }, {
+      t("```"),
+      i(1, "py"),
+      t({ "", "" }),
+      i(2),
+      t({ "", "```", "" }),
+      i(0),
+    }),
+  -- Footnote
+  s({ trig = "fnt", snippetType = "autosnippet" }, {
+      t("[^"),
+      i(1, "Footnote"),
+      t("]"),
+      i(0),
+      t({
+        "",
+        "",
+        "[^",
+      }),
+      i(1),
+      t("]: "),
+      i(2, "Text"),
+    }),
+  -- Disclosure
+  s({ trig = "dl" }, {
+      t({
+        "<details open=\"\">",
+        "  <summary>",
+      }),
+      i(1),
+      t({
+        "</summary>",
+        "  ",
+      }),
+      i(2),
+      t({
+        "",
+        "</details>",
+      }),
+      i(0),
+    }),
+  -- Link
+  s({
+      trig = "lnk",
+      priority = 1000,
+      snippetType = "autosnippet",
+    }, {
+      t("["),
+      i(1),
+      t("]("),
+      i(2, "link"),
+      t(")"),
+      i(0),
+    }),
+  -- Code block (python)
+  s({ trig = "cd", snippetType = "autosnippet" }, {
+      t("```python"),
+      t({ "", "" }),
+      i(1),
+      t({ "", "```", "" }),
+      i(0),
+    }),
+  -- Horizontal code block (updated mc variant ignored earlier)
+  s({ trig = "bk", snippetType = "autosnippet" }, {
+      t("<br>"),
+      i(0),
+    }),
+  -- Section headers
+  s({ trig = "sec", snippetType = "autosnippet" }, {
+      t("# "),
+      i(1, "Section Name"),
+    }),
+  s({ trig = "sub", snippetType = "autosnippet" }, {
+      t("## "),
+      i(1, "Section Name"),
+    }),
+  s({ trig = "sbb", snippetType = "autosnippet" }, {
+      t("### "),
+      i(1, "Section Name"),
+    }),
+  s({ trig = "par", snippetType = "autosnippet" }, {
+      t("#### "),
+      i(1),
+    }),
+  s({ trig = "spar", snippetType = "autosnippet" }, {
+      t("##### "),
+      i(1),
+    }),
+  -- Formatting
+  s({ trig = "bf", snippetType = "autosnippet" }, {
+      t("**"),
+      i(1),
+      t("**"),
+      i(2),
+    }),
+  s({ trig = "itl", snippetType = "autosnippet" }, {
+      t("_"),
+      i(1),
+      t("_"),
+      i(2),
+    }),
+  -- Equation block
+  s({ trig = "eqn", snippetType = "autosnippet" }, {
+      t({ "$$", "" }),
+      i(1),
+      t({ "", "$$", "" }),
+      i(0),
+    }),
+  -- Markdown break
+  s({ trig = "bk", snippetType = "autosnippet" }, {
+      t("<br>"),
+      i(0),
+    }),
+  -- Anki card
+  s({ trig = "ak", snippetType = "autosnippet" }, {
+      t("> **_"),
+      i(1, "NOTE:"),
+      t("_** <br>"),
+      t({ "", "" }),
+      i(2),
+      t("<br>"),
+      i(0),
+    }),
+  -- cloze-style block
+  s({ trig = "clz", snippetType = "autosnippet" }, {
+      t("> "),
+      i(1),
+      t({ "", "" }),
+      i(0),
+    }),
+  -- Checklist (your original ckls)
+  s({
+      trig = "ckls",
+      snippetType = "autosnippet",
+      wordTrig = false,
+    }, {
+      t("- [ ] "),
+      i(1),
+    }),
+}
